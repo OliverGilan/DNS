196ecf68fae73c85d72ab6cbc0f2d16c57bb6251
Nicolas Carchio <ncarchio@gmail.com>
2020-02-18 11:47:48 -0500

    TS branch initial commit

---
+++ ts.py
@@ -0,0 +1,15 @@
+'''
+
+	Top-Level DNS server (TS)
+	- maintains a DNS_table of three fields: 
+		- Hostname
+		- IP address
+		- Flag (A or NS)
+	1. reads in DNS records from a file 
+		- dictionary to maintain the table 
+	2. client sends queried hostname as a string to TS
+	3. The TS program looks up the hostname in its DNS_table (dictionary) 
+		3a. if there is a match, we sent the entry as a string
+		3b. if there is no match, we send an error message: "Error:HOST NOT FOUND"
+
+'''
