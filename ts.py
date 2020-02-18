06684aae9568880163b02f6f61bb6514ab6e04e5
Nicolas Carchio <ncarchio@gmail.com>
2020-02-18 16:03:55 -0500

    before testing

--- ts.py
+++ ts.py
@@ -17,6 +17,8 @@ import socket
 import select
 import sys
 
+max_buffer = 100000
+
 def ts(): 
 	# set up dictionary and read in from file
 	dns = {}
@@ -33,13 +35,24 @@ def ts():
 	       	dns[host] = line
 
 	#  establish socket and start listening on the port
-	ts_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	ts_server.bind('', 19190)
+	ts_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	ts_socket.bind('', 19190)
+	print("TS server has been initialized and is listening for connections on port: 19190")
+		
+	# listening loop
+	while (True):	
+		# get client message
+		msg, addr = ts_socket.recvfrom(max_buffer)
+		decoded_msg = msg.decode()
 
-		# get client message 
+		# find in dictionary
+		tokens = decoded_msg.split()
+		# send the full message to the client, else send an error
+		if dns[tokens[0]]: 
+			ts_socket.sendto(dns[tokens[0]], addr)
+		else: 
+			ts_socket.sendto("Error:HOST NOT FOUND", addr)
 
-		# look it up in dictionary 
 
-		# give response
 
 
